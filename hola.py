bf9ce2d35c80d102a3ae3fc59c0763990ef675a9
rodrigo <rodrigodlfc@gmail.com>
2026-07-08 20:50:11 +0200

    Second modification to hola.py

--- hola.py
+++ hola.py
@@ -2,3 +2,4 @@ import sys
 
 print("Hola, Road to Data Analyst 🚀")
 print(sys.executable) # This line prints the path of the Python interpreter that is currently being used to run the script.
+print("Segundo día aprendiendo Git")
