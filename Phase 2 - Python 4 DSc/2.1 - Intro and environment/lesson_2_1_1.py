fb3d482464f17d4f0170331000c52105bdcdee54
rodrigo <rodrigodlfc@gmail.com>
2026-08-10 19:05:51 +0200

    Add python script for lesson 2.1.1

---
+++ Phase 2 - Python 4 DSc/2.1 - Intro and environment/lesson_2_1_1.py
@@ -0,0 +1,10 @@
+# un pequeño programa que muestre:
+# El nombre de tu proyecto
+# El nombre de un dataset que quieras utilizar
+# El objetivo del análisis
+
+project_name = 'Data Analyzer'
+dataset_name = 'movies.csv'
+objective = 'Analyze and explore movie data'
+
+print(project_name,'\n',dataset_name, '\n',objective)
