f60f56bd6c24f3537d37377ae050b12087c4e6cb
rodrigo <rodrigodlfc@gmail.com>
2026-08-10 19:10:39 +0200

    Add lesson_2_1_2.py

---
+++ Phase 2 - Python 4 DSc/2.1 - Intro and environment/lesson_2_1_2.py
@@ -0,0 +1,9 @@
+dataset_name = 'movies.csv'
+registry_number = 1500
+average_rating = 7.5
+is_dataset_uploaded = True
+
+print(type(dataset_name),'\n',
+      type(registry_number),'\n',
+      type(average_rating),'\n',
+      type(is_dataset_uploaded))
