d4f20fe3a3c644ba5a7097f4ba550432aec69a82
rodrigo <rodrigodlfc@gmail.com>
2026-09-08 12:50:44 +0200

    Finish practical

---
+++ Phase 2 - Python 4 DSc/2.3 - Estructuras de datos/lesson_2_3_1.py
@@ -0,0 +1,7 @@
+habilidades = ['Python', 'SQL', 'Power BI', 'Tableau', 'Deep Learning']
+print(habilidades[0])  # Imprime primera habilidad
+print(habilidades[-1])  # Imprime última habilidad
+print(len(habilidades))  # Imprime el total de habilidades
+
+habilidades.append('Machine Learning')  # Agrega una nueva habilidad al final
+print(habilidades)
